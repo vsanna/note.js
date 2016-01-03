7eb7df0dd0d45ce77e7030ec8793900e03713971
Ryu Ishikawa <ryu.ishikawa@howtelevision.jp>
2016-01-03 13:21:17 +0900

    change readme

--- note.js
+++ note.js
@@ -1,44 +1,3 @@
-/*
-# javascriptライブラリの練習
-
-1. 指定したDOMを編集可能(contentEditable)にする
-2. 押下したkeyCodeをconsoleに流す
-3. localStorageへの保存と読み出し
-4. 初期読み出しができる. また、初期読み出しにおいて直前に編集していたテキストを読み出せる
-4. 2を元にショートカットを表示
-*/
-
-// 書き方1 クロージャーにしたうえでconstructorを返す
-// var TestModule = (function(){
-//   var constructor = function(config){
-//     this.selector = config.selector
-//   }
-//
-//   constructor.prototype.run = function(){
-//     findEditableArea.call(this)
-//     makeEditableArea.call(this)
-//     showWitchKeyIsPressing.call(this)
-//   }
-//
-//   var findEditableArea = function(){
-//     this.editableArea = document.querySelector(this.selector)
-//     console.log(this)
-//   }
-//   var makeEditableArea = function(){
-//     this.editableArea.contentEditable = true
-//   }
-//
-//   ... 一部省略
-//
-//   var showWitchKeyIsPressing = function(){
-//     this.editableArea.addEventListener('keypress', function(e){ console.log(e.keyCode) })
-//   }
-//
-//   return constructor
-// })()
-
-
-// 書き方2 オーソドックス.
 var Note = function(config){
   this.MAX_ARTICLE_NUM = 20;
   this.KEY_PREFIX = (config.prefix) ? config.prefix : 'Note';
@@ -61,11 +20,6 @@ var Note = function(config){
 
 Note.prototype = {
   run: function(){
-    // このthisはrunを呼び出したインスタンス
-
-    // 書き方1
-    // インスタンスは直接はこのメソッドを持たないため、this.__proto__(==TestModule.prototype)を見に行っている.
-    // なお、呼び出した関数内でのthisは関数呼び出しを行ったインスタンスそのものなので問題なし
     this.findEditableArea();
     this.makeEditableArea();
     this.makeEditableAreaFocus();
@@ -75,23 +29,6 @@ Note.prototype = {
     this.makeButtonToSave();
 
     this.showWitchKeyIsPressed();
-    // this.renderMD();
-
-    // 書き方2
-    // this.__proto__を明示的に書いたもの. 普段使いしない
-    // this.__proto__は、継承元.prototypeへの暗黙のリンク
-    // 呼び出しがTestModule.prototypeになってしまっているので, call(this)をして、ここでのthis = runを実行したインスタンスをbindしている
-    // TestModule.prototype.findEditableArea.call(this); // TestModule.prototype.findEditableArea()は当然エラー
-    // TestModule.prototype.makeEditableArea.call(this);
-    // TestModule.prototype.showWitchKeyIsPressing.call(this);
-
-    // 書き方3
-    // もちろんこんな書き方はしない
-    // この時、関数を呼び出しているのが、this.__proto__(===TestModule)であるため、よびだした関数内でのthisがインスタンスを示さない。
-    // よって、ここでのthis = runを実行したインスタンスをcallでbindしている
-    // this.__proto__.findEditableArea.call(this);
-    // this.__proto__.makeEditableArea.call(this);
-    // this.__proto__.showWitchKeyIsPressing.call(this);
   },
   findEditableArea: function(){
     console.log(this.selector)
@@ -126,7 +63,7 @@ Note.prototype = {
   makeButtonToSave: function(){
     this.editableArea.addEventListener('keyup', (function(){
       localStorage.setItem(this.edittingKey, this.editableArea.innerHTML);
-    }).bind(this)) // bind(this)することで、イベント付与時に、thisがDOM要素になることを回避!非常に便利
+    }).bind(this))
   },
   makeButtonToCreateNewArticle: function(){
     document.querySelector('.btn_create').addEventListener('click', (function(){
@@ -187,11 +124,7 @@ Note.prototype = {
     btn.setAttribute('class', 'btn_switch');
     document.getElementById('menu').appendChild(li);
 
-    // ボタンにイベント付与
     btn.addEventListener('click', this.switchText.bind(this, dataKey));
-    // call, applyは関数の『実行』
-    // bindは関数内のthisと引数を固定した関数を返す
-    // method.bind(thisの指すobj, param1, ...)
 
     var deleteBtn = document.createElement('button')
     deleteBtn.appendChild(document.createTextNode('delete'));
